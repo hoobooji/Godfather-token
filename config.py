3534e0aa4dbc7c96d71522ae1a0bab44b72070ad
hoobooji <162422722+hoobooji@users.noreply.github.com>
2024-04-21 02:44:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = "mongodb+srv://mrjackjackiechan:mrjackjackiechan@cluster0.cojfvuo.mongodb.net/?retryWrites=true&w=majority"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "a8f5b3abb4b6c78581c3d96ec5049f3047bee268")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "urlshortx.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "1a40e2e396ffc722c4b22b2b91a14efe846b0448")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","leakhubd/1264")
